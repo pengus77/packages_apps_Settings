d288cc3111bf395991955b2878769dfdc90655c0
mikeNG <mikeioannina@gmail.com>
2020-11-08 14:59:01 +0200

    Settings: Use correct icon for notification volume
    idoybh: Made it for notification volume instead of ringer
    Use headsup drawable to match base

--- src/com/android/settings/notification/NotificationVolumePreferenceController.java
+++ src/com/android/settings/notification/NotificationVolumePreferenceController.java
@@ -66,15 +66,8 @@ public class NotificationVolumePreferenceController extends
     @Override
     protected void updatePreferenceIcon() {
         if (mPreference != null) {
-            if (mRingerMode == AudioManager.RINGER_MODE_VIBRATE) {
-                mMuteIcon = R.drawable.ic_volume_ringer_vibrate;
-                mPreference.showIcon(R.drawable.ic_volume_ringer_vibrate);
-            } else if (mRingerMode == AudioManager.RINGER_MODE_SILENT) {
-                mMuteIcon = R.drawable.ic_notifications_off_24dp;
-                mPreference.showIcon(R.drawable.ic_notifications_off_24dp);
-            } else {
-                mPreference.showIcon(R.drawable.ic_notifications);
-            }
+            mPreference.showIcon(R.drawable.ic_audio_notification);
         }
     }
+
 }
